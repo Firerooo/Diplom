8c0e95bb7ca7e9b4e00d36bfcc058df7f83d9789
Viktor <vitya.gusakov.00@gmail.com>
2025-03-24 17:53:19 +0300

    2 commit, add 2 pages

--- rental/listings/views.py
+++ rental/listings/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
-# Create your views here.
+def home(request):
+    return render(request, 'listings/home.html')
+
+def about(request):
+    return render(request, 'listings/about-us.html')
+
+
--- rental/rental/settings.py
+++ rental/rental/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'rental.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "listings/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
